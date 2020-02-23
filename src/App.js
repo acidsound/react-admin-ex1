90b17c51a0ea4e1477f8132fb0674fe1cd15b926
acidsound <spectrick@gmail.com>
2020-02-24 00:06:37 +0900

    add Posts and referenced field

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
-import { UserList } from './users'
+import { Admin, Resource, EditGuesser } from 'react-admin';
+import { UserList } from './users';
+import { PostList, PostEdit } from './posts';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('//jsonplaceholder.typicode.com');
 
 export default () => (
   <Admin dataProvider={dataProvider}>
-      <Resource name="users" list={UserList} />
+      <Resource name="users" list={UserList} edit={EditGuesser} />
+      <Resource name="posts" list={PostList} edit={PostEdit} />
   </Admin>
 );
---
+++ src/posts.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { List, Datagrid, ReferenceField, TextField, SimpleForm, Edit, ReferenceInput, SelectInput, TextInput } from 'react-admin';
+
+export const PostList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <ReferenceField source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
+            <TextField source="title" />
+            <TextField source="body" />
+        </Datagrid>
+    </List>
+);
+
+export const PostEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput source="body" />
+        </SimpleForm>
+    </Edit>
+);
