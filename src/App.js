2e4b6be412f8485b6caa74d599156bcb1c27a0d6
acidsound <spectrick@gmail.com>
2020-02-24 02:36:07 +0900

    apply dashboard and theme, authProvider

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { Admin, Resource, EditGuesser } from 'react-admin';
 import { UserList } from './users';
 import { PostList, PostEdit } from './posts';
 import jsonServerProvider from 'ra-data-json-server';
+import authProvider from './authProvider';
+import theme from './themes/Paperbase';
+import dashboard from './dashboard';
 
 const dataProvider = jsonServerProvider('//jsonplaceholder.typicode.com');
 
 export default () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin theme={theme} dataProvider={dataProvider} authProvider={authProvider} dashboard={dashboard}>
       <Resource name="users" list={UserList} edit={EditGuesser} />
       <Resource name="posts" list={PostList} edit={PostEdit} />
   </Admin>
---
+++ src/dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { Title } from 'react-admin';
+export default () => (
+    <Card>
+        <Title title="Yet another admin" />
+        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+    </Card>
+);
